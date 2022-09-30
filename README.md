d621e49d19fa8cbfe2eacb7d73e0d591d8fae8cf
radex891 <radex891@gmail.com>
2022-09-30 19:29:07 +0200

    finished Videos + updated KinfuResults

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Authors: Timo Burger, Nicolas Krosta
 Before cloning this repository install and configure Git Large File Storage. For more information use this link: [Git Large File Storage](https://docs.github.com/en/repositories/working-with-files/managing-large-files/configuring-git-large-file-storage)
 
 Please use "git clone https://github.com/username/KinfuThermal.git" to download this repository. Downloading the .zip file does not support Git Large File Storage.
+
+
+
---
+++ code/python Code/makeVideo.py
@@ -0,0 +1,38 @@
+import cv2
+import glob
+import sys, getopt
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", "ifile=")
+    except getopt.GetoptError:
+        print('makeVideo.py -i <my_picture_sequence>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('----no options----\nmakeVideo.py -i <my_picture_sequence>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            bagfile = arg
+    path_and_name = str(bagfile)
+    if path_and_name[0] != '/':
+        path_and_name = '/' + path_and_name
+    if path_and_name[-1] != '/':
+        path_and_name = path_and_name + '/'
+    print('video is being rendered........')
+    img_array = []
+    filenames = sorted(glob.glob(path_and_name + '*.png'))
+    for filename in filenames:
+        print(filename)
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        img_array.append(img)
+    out = cv2.VideoWriter(path_and_name + 'out.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 15, size)
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
